1d6e85a91dc7c4fb1f8cf2b9d3f2862884dbadac
Noyrven <zaharowroman@gmail.com>
2020-11-27 12:53:39 +0100

    need fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,45 @@ import "./main.css";
 
 function App() {
   const [difficulty, setDifficulty] = useState(6),
-        [colorsArray, setColors] = useState(generateColors(6)),
-        [secretColor, setSecret] = useState(colorsArray[getRandomNum(6)]),
-        [gameState, setGameState] = useState('start');
+    [colorsArray, setColors] = useState(generateColors(6)),
+    [secretColor, setSecret] = useState(colorsArray[getRandomNum(5)]),
+    [gameState, setGameState] = useState("start");
 
-  useEffect(() => {
+  //Change to useReducer
+  // useEffect(() => {
+  //   setColors(generateColors(difficulty));
+  // }, [difficulty]);
+  // useEffect(() => {
+  //   setSecret(colorsArray[getRandomNum(difficulty - 1)]);
+  // }, [colorsArray]);
+  ///
+
+  const checkMatch = e => {
+    const tile = e.target.style
+    if (secretColor === tile.background) {
+      setGameState("won");
+      setColors(Array.from(new Array(difficulty), () => secretColor));
+    } else {
+      setGameState("wrong");
+      tile.background = "#131313";
+      tile.cursor = "default"
+    }
+  };
+
+  const newGame = () => {
+    setGameState('start');
     setColors(generateColors(difficulty));
-    setSecret(colorsArray[getRandomNum(difficulty)])
-  }, [difficulty]); //Change to useReducer 
+    setSecret(colorsArray[getRandomNum(difficulty - 1)]);
+  }
 
   const tiles = colorsArray.map((color, i) => {
     return (
       <div
-      key={i}
-      className="tile"
-      style={{
-        background: color,
-      }}
-    ></div>
+        key={i}
+        className="tile"
+        style={{ background: color, cursor: gameState !== 'won' ? 'pointer' : 'default' }}
+        onClick={checkMatch}
+      ></div>
     )
   });
 
@@ -31,14 +52,15 @@ function App() {
       <h2 className="color"> {secretColor} </h2>
       <h2>is your color for this round</h2>
 
-      <button onClick={() => setDifficulty(6)}>EASY</button>
-      <button onClick={() => setDifficulty(9)}>HARD</button>
-
-      <div className="container">
-        {tiles}
+      <div>
+        <button onClick={() => { setDifficulty(6); setGameState('start') }}>EASY</button>
+        <button onClick={() => { setDifficulty(9); setGameState('start') }}>HARD</button>
+        <button onClick={() => newGame()}>NEW ROUND</button>
       </div>
+
+      <div className="container">{tiles}</div>
     </>
-  );
+  )
 }
 
 export default App;
